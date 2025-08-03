a1e99226cc219d066335199c80503645daa114c3
violet <hi@vio.vin>
2025-08-03 14:41:52 +0800

    feat: change outdate

--- src/components/post/Outdate.tsx
+++ src/components/post/Outdate.tsx
@@ -22,7 +22,7 @@ export function Outdate({ lastMod }: { lastMod: Date }) {
           exit={{ opacity: 0 }}
         >
           <span>
-            这篇文章最后修改于 {getFormattedDate(lastMod)}
+            这篇文章最后发布于 {getFormattedDate(lastMod)}
             ，部分内容可能已经不适用，如有疑问可联系作者。
           </span>
         </motion.div>
